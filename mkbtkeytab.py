b21dfb44e56aaa592217e1c0297b44c89a665a76
Terminus <i.can.speak.c.and.basic@gmail.com>
2014-02-25 19:40:41 +0900

    mkbtkeytab.py: change to use new key table format

--- mkbtkeytab.py
+++ mkbtkeytab.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 import sys
 
-table=[[-1 for y in range(2)]for x in range(0xff)]
+table=[[-1 for y in range(2)]for x in range(0xE7+1)]
 
 def outputTable():
-	for f in range(0xff-1):
+	for f in range(0xE7+1):
 		print('\t{%d,\t%d},\t/* 0x%02X */'%(table[f][0], table[f][1], f))
-	print('\t{%d,\t%d}\t/* 0x%02X */'%(table[0xff-1][0], table[0xff-1][1], 0xff-1))
+	print('\t{%d,\t%d}\t/* 0x%02X */'%(table[0xE7][0], table[0xE7][1], 0xE7))
 
 def processNormalKeyLine(l):
 	table[l[0]][0]=l[2]
@@ -19,21 +19,17 @@ def main():
 		except EOFError:
 			break
 
-		if len(s) == 0:
+		if len(s) == 0 or s[0] == '#':
 			continue
 
 		l=s.split()
 		if len(l) != 3:
-			print("Error. len of the line \"%s\" is not 3."%(s), file=sys.stderr)
-			print('Skipping.', file=sys.stderr)
+			print("Error. len of the line \"%s\" is not 3. Skipping"%(s), file=sys.stderr)
 		else:
 			try:
-				if l[0][0]=='\\' and l[0][1]!='\\':
-					l[0]=int('0x'+l[0][1:], 0)
-				else:
-					l[0]=ord(l[0][0]) #for \\
-				l[1]=int(l[1])
-				l[2]=int(l[2])
+				l[0]=int(l[0], base=16)
+				l[1]=int(l[1], base=16)
+				l[2]=int(l[2], base=16)
 			except ValueError:
 				print("On line \"%s\""%(s), file=sys.stderr)
 				print("There are some invalid value.", file=sys.stderr)
@@ -41,7 +37,7 @@ def main():
 
 			processNormalKeyLine(l)
 
-	print('int8_t table_normal_keys[%d][2]={'%(0xff))
+	print('int8_t table_normal_keys[%d][2]={'%(0xE7+1))
 	print('\t/* rows and columns */')
 	print('\t/* Note that -1 means not-implemented */')
 	outputTable()
